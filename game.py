8a30a14813278eda1dade150fd4608d8c54f9d6d
kmorto <khalib76@gmail.com>
2020-11-20 22:22:14 -0500

    Added snake

--- game.py
+++ game.py
@@ -1,13 +1,46 @@
 import pygame
 
 pygame.init()
-dis=pygame.display.set_mode((400,300))
+dis = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Snake AI Bro')
-while True:
+
+red = (255, 0, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+
+# Starting pos
+x1 = 200
+y1 = 150
+
+#to move the snake on key press
+x_change = 0
+y_change = 0
+
+clock = pygame.time.Clock()
+
+game_over = False
+while not game_over:
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN: #Moves the snake UP DOWN LEFT or RIGHT on key press
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                x_change = -10
+                y_change = 0
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                x_change = 10
+                y_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                y_change = -10
+                x_change = 0
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                y_change = 10
+                x_change = 0
+    x1 += x_change
+    y1 += y_change
+    dis.fill(black)
+    pygame.draw.rect(dis,red, [x1, y1, 10, 10])
+    pygame.display.update();
+    clock.tick(30)
 pygame.quit()
 quit()
-
-#adding in a comment
-#or two!
