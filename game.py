51a10a9c4a7e8546ee2d1a37ee28b9cb44af61d7
kmorto <khalib76@gmail.com>
2020-11-20 21:46:02 -0500

    file for base game

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+import pygame
+
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+pygame.display.set_caption('Snake AI Babey')
+while True:
+    for event in pygame.event.get():
+        print(event)
+pygame.quit()
+quit()
 
