5061e04653a74f7f317d672efccdd57fe8af7867
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2020-11-20 21:51:16 -0500

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 dis=pygame.display.set_mode((400,300))
-pygame.display.set_caption('Snake AI Babey')
+pygame.display.set_caption('Snake AI Bro')
 while True:
     for event in pygame.event.get():
         print(event)
