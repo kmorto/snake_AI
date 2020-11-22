aca6e76433dce0dfeb185e1e23d87b2ec3caca9e
kmorto <khalib76@gmail.com>
2020-11-21 20:33:38 -0500

    added boundaries

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import pygame
 
 pygame.init()
-dis = pygame.display.set_mode((400, 300))
+disx = 400
+disy = 300
+dis = pygame.display.set_mode((disx,disy))
 pygame.display.set_caption('Snake AI Bro')
 
 red = (255, 0, 0)
@@ -9,8 +11,8 @@ blue = (0, 0, 255)
 black = (0, 0, 0)
 
 # Starting pos
-x1 = 200
-y1 = 150
+x1 = disx / 2
+y1 = disy / 2
 
 #to move the snake on key press
 x_change = 0
@@ -38,9 +40,13 @@ while not game_over:
                 x_change = 0
     x1 += x_change
     y1 += y_change
+    if x1 > disx or x1 < 0:
+        game_over = True
+    if y1 > disy or y1 < 0:
+        game_over = True
     dis.fill(black)
     pygame.draw.rect(dis,red, [x1, y1, 10, 10])
-    pygame.display.update();
-    clock.tick(30)
+    pygame.display.update()
+    clock.tick(25)
 pygame.quit()
 quit()
